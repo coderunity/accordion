49db4f21ff67a0faf0760e0ddd8d1eeb64ad7f78
developerraihant <developerraihant@gmail.com>
2022-02-05 12:17:44 +0600

    replace a new file

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
 
-const 
+const a_header = document.querySelectorAll('.accodion-header');
+
+a_header.forEach(item => {
+
+    item.addEventListener('click', function(){
+
+        a_header.forEach(item =>{
+            item.classList.remove('active');
+            item.nextElementSibling.style.height = '0px'
+        });
+
+        item.classList.toggle('active');
+        
+        
+
+
+        if(item.classList.contains('active')){
+            item.nextElementSibling.style.height =  item.nextElementSibling.scrollHeight + 'px'; 
+        }else{
+            item.nextElementSibling.style.height = '0px'
+        }
+
+
+    });
+
+});
